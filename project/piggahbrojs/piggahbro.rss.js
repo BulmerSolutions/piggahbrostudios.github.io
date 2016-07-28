e4eef81eaaa76e8ac4acc055e5edf753a4597171
Piggah Bro <piggahbro@gmail.com>
2016-07-28 08:31:30 -0400

    Update piggahbro.rss.js

--- project/piggahbrojs/piggahbro.rss.js
+++ project/piggahbrojs/piggahbro.rss.js
@@ -17,7 +17,7 @@ PB.rss = function (){
   for(i=0; i < this.feed.length; i++){
     PB.server.getFile('GET', {
       url: this.feed[i].getAttribute('url'),
-      headers: ['Content-Type', 'text/js'],
+      headers: ['Access-Control-Allow-Origin', '*'],
       onready: function(xhttp){
         if (xhttp.status === 200) {
           alert(xhttp.responseText);
